5e5689fe6057bea516c8d62831dddc2dc26aaba6
GaneshN523 <ganeshnalawade523@gmail.com>
2024-12-16 00:40:53 +0530

    add

--- modu.py
+++ modu.py
@@ -2,5 +2,8 @@
 
 def fun(num, num2):
     return num + num2
+num = 1
+num2 = 4
+sum = (num, num2)
+print(f"This is the first file of python coding practice i've been doing since last few days & by the way this is the sum of the two entered numbers {sum}")
 
-print("This is the first file of python coding practice i've been doing since last few days")
