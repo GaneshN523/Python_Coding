25cf8c72d829c5e28a2890f3fa3676259bb1aee6
GaneshN523 <ganeshnalawade523@gmail.com>
2024-12-16 00:43:28 +0530

    the new change in modu

--- modu.py
+++ modu.py
@@ -3,4 +3,4 @@
 def fun(num, num2):
     return num + num2
 
-print("This is the first file of python coding practice i've been doing since last few days")
+print("The above function gives sum of the two entered numbers")
